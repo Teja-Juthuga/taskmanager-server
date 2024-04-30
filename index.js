2bb0717a983b904546f9a05172b3718bc7cf8d87
Teja Juthuga <tejajuthuga@gmail.com>
2024-04-30 22:39:30 +0530

    developed updateTask Route

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const test = require("./routes/test.route");
 const createNewTask = require("./routes/createTask.route");
 const getALLTasks = require("./routes/getAllTasks.route");
 const getTask = require("./routes/getTask.route");
-
+const updateTask = require("./routes/updateTask.route");
 
 app.use("/tasks", createNewTask);
 app.use("/tasks", getALLTasks);
 app.use("/tasks", getTask);
+app.use("/tasks", updateTask);
 app.use("/Test", test);
 
 
---
+++ routes/updateTask.route.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../database/connectDB");
+
+router.put("/:id", async (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({ error: 'Task details are missing in the request body' });
+    }
+
+    const taskId = req.params.id;
+    
+    const taskDetails = req.body;
+    const {
+        title, 
+        description,
+        status, 
+        assignee_id, 
+        created_at, 
+        updated_at
+    } = taskDetails;
+    const addTaskQuery = `
+        UPDATE 
+            Tasks 
+        SET
+            title='${title}', 
+            description='${description}',
+            status='${status}', 
+            assignee_id=${assignee_id}, 
+            created_at=${created_at}, 
+            updated_at=${updated_at}
+        WHERE
+            id=${taskId}
+    `;
+    pool.query(addTaskQuery, (err, result)=>{
+        if (err){
+            console.log('Error: ' + err);
+            res.status(500).send('Error while inserting data into database');
+        }else{
+            
+            // console.log('query: ' + addTaskQuery);
+            // console.log("Result: " + result);
+            res.send({'response': "Updated Successfully"});
+        }
+    })    
+});
+
+module.exports = router;
